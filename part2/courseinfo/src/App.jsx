3b2019c3fbd90c5cacc0b99f10619b8ba2c1737c
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-20 12:32:56 +0530

    Completed Ex2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 
+const Header =({text}) =>{
+  return(
+    <h1>{text}</h1>
+  )
+}
+
+const Content =({parts}) =>{
+  return(
+    <>
+    {parts.map(part => <p>{part.name} {part.exercises}</p>)}
+
+    </>
+  )
+}
+
+const Course =({course}) =>{
+  return(
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
 const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
   return (
-    <div>App</div>
+    <Course course={course} />
   )
 }
 
