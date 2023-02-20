a5cc02b2cc88c47ff78d77548021af2ef6f02fa3
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-20 13:27:43 +0530

    Completed Ex2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName,setNewName] = useState('')
+
+  const handleNameChange =(e) =>{
+    e.preventDefault()
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) =>{
+    e.preventDefault();
+    const person = {name:newName};
+    setPersons(persons.concat(person))
+  }
   return (
-    <div>App</div>
+    <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addPerson}>
+        <div>
+          name : 
+          <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <button type='submit'>add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => {
+        return <p>{person.name}</p>
+      })}
+    </div>
   )
 }
 
