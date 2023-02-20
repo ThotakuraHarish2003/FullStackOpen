bc0f3320620f4ae2f47121179ccbb6356775f839
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-20 13:46:24 +0530

    Completed Ex2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,20 +2,28 @@ import React,{useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number : "00000000"
+  }
   ]) 
   const [newName,setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState('')
 
   const handleNameChange =(e) =>{
     e.preventDefault()
     setNewName(e.target.value)
   }
 
+  const handleNumberChange =(e) =>{
+    e.preventDefault()
+    setNewNumber(e.target.value)
+  }
+
   const addPerson = (e) =>{
     e.preventDefault();
     const filteredPerson = persons.filter(person => person.name ==newName);
     if(filteredPerson.length==0){
-      const person = {name:newName};
+      const person = {name:newName,number:newNumber};
       setPersons(persons.concat(person))
     }
     else{
@@ -31,11 +39,15 @@ const App = () => {
           name : 
           <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>
+          number :
+          <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         <button type='submit'>add</button>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => {
-        return <p>{person.name}</p>
+        return <p>{person.name}:{person.number}</p>
       })}
     </div>
   )
