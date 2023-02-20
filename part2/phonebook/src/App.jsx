163a9c46511e70de3b72c6706d5c0772b33fdefc
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-20 14:04:05 +0530

    Completed Ex2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,15 @@ import React,{useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number : "00000000"
-  }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [displayPersons,setDisplayPersons] = useState(persons); 
   const [newName,setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
+  const [filter,setFilter] = useState('')
 
   const handleNameChange =(e) =>{
     e.preventDefault()
@@ -19,12 +22,23 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleFilterChange = (e) =>{
+    e.preventDefault()
+    setFilter(e.target.value)
+    const reg = new RegExp(e.target.value,'i')
+    const filteredPerson = persons.filter(person=>person.name.match(reg))
+    setDisplayPersons(filteredPerson)
+    
+  }
+
   const addPerson = (e) =>{
     e.preventDefault();
     const filteredPerson = persons.filter(person => person.name ==newName);
     if(filteredPerson.length==0){
       const person = {name:newName,number:newNumber};
+      setDisplayPersons(persons.concat(person))
       setPersons(persons.concat(person))
+      
     }
     else{
       window.alert(`${newName} is already added to phonebook`)
@@ -34,6 +48,10 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <div>
+        filter show with <input value={filter} onChange={handleFilterChange}/>
+      </div>
+      <h1>add a new</h1>
       <form onSubmit={addPerson}>
         <div>
           name : 
@@ -46,7 +64,7 @@ const App = () => {
         <button type='submit'>add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
+      {displayPersons.map(person => {
         return <p>{person.name}:{person.number}</p>
       })}
     </div>
