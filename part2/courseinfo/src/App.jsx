90a21a1b6a76bda6f3265c2bb2bb3f650ed77d16
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-20 12:39:21 +0530

    Completed Ex2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -10,7 +10,9 @@ const Content =({parts}) =>{
   return(
     <>
     {parts.map(part => <p>{part.name} {part.exercises}</p>)}
-
+    <p><b>total of  {parts.reduce((sum,p)=>{
+      return sum+=p.exercises
+    },0)}</b></p>
     </>
   )
 }
