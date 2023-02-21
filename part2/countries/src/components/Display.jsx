f4d81067ee9cef265cd0dae4d936a85ae1cdb4e8
ThotakuraHarish2003 <123625549+ThotakuraHarish2003@users.noreply.github.com>
2023-02-21 10:23:45 +0530

    Completed Ex2.20

--- part2/countries/src/components/Display.jsx
+++ part2/countries/src/components/Display.jsx
@@ -7,24 +7,29 @@ const Display = ({countries}) => {
     const onShow = (id) =>{
         setCountry(countries[id])
     }
-
-    if(countries.length>10){
-        return <p>Too many matches, specify another filter</p>
-    }
     if(countries.length==1){
         return <Country country={countries[0]} />
     }
-    return (
-        <div>
-            {countries.map((country,id)=>
-            <p>
-                {country.name.common}
-                <button onClick={()=>onShow(id)}>show</button>
-                </p>
-            )}
-            <Country country={country} />
-        </div>
-      )
+    else if(countries.length==0){
+        return <p>No countries found</p>
+    }
+    else if(countries.length>10){
+        return <p>Too many matches, specify another filter</p>
+    }
+    else{
+        return (
+            <div>
+                {countries.map((country,id)=>
+                <p>
+                    {country.name.common}
+                    <button onClick={()=>onShow(id)}>show</button>
+                    </p>
+                )}
+                <Country country={country} />
+            </div>
+          )
+    }
+    
   
 }
 
--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -2,8 +2,8 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
 const Weather = ({city}) => {
-    const [weather,setWeather] = useState('')
-    const appid = "ee"
+    const [weather,setWeather] = useState([])
+    const appid ="ee"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appid}`
     const hook = () =>{
         axios.get(url)
@@ -11,10 +11,22 @@ const Weather = ({city}) => {
                 setWeather(response.data);
                 console.log(response)
             })
+            .catch(error=>{
+                console.log(error)
+            })
     }
     useEffect(hook,[])
+
+    if(weather.length==0){
+        return(<></>)
+    }
   return (
-    <div>Weather</div>
+    <div>
+        <h1>Weather in {city}</h1>
+        <p>temperature {weather.main.temp}</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+        <p>wind {weather.wind.speed} m/s</p>
+    </div>
   )
 }
 
